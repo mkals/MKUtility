967d15d43b50534f2c98a74279be4a651eeef84c
Morten Kals <m.kals@me.com>
2016-03-06 14:00:08 +0100

    Initial Commit

---
+++ MKUtility/MKUtility.h
@@ -0,0 +1,19 @@
+//
+//  MKUtility.h
+//  MKUtility
+//
+//  Created by Morten Kals on 06/03/16.
+//  Copyright © 2016 Morten Kals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKUtility.
+FOUNDATION_EXPORT double MKUtilityVersionNumber;
+
+//! Project version string for MKUtility.
+FOUNDATION_EXPORT const unsigned char MKUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKUtility/PublicHeader.h>
+
+
